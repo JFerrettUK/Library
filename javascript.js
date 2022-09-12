3a98eb81955116919bfd1e0da7462efd98277e75
JFerrettUK <jferrettuk@gmail.com>
2022-09-12 20:13:26 +0100

    Library javascript function added to close cards

--- javascript.js
+++ javascript.js
@@ -6,18 +6,15 @@ function closeform() {
 }
 
 // Stops Submit button from refreshing the page
-
 let myForm = document.getElementById("myForm")
 myForm.addEventListener("submit", (e) => {
         console.log("Form has been submitted")
         e.preventDefault();
 });
 
-// Check if tickbox was clicked
-
+// Check if tickbox was clicked, off by default
 let isRead = "off";
 
-    
 let myLibrary = [];
 
 function book(title, author, pages, isRead) {
@@ -27,26 +24,43 @@ function book(title, author, pages, isRead) {
         this.isRead = isRead;
 }
 
-function addBookToLibrary(isRead) {
-        let author = document.getElementById('authorInput').value;
-        let title = document.getElementById('titleInput').value;
-        let pages = document.getElementById('pgNoInput').value;
-        function validate(){
-                var remember = document.getElementById('completedInput');
-                if (remember.checked){
-                        isRead = "on";
-                        return isRead;
-                } else {
-                        isRead = "off";
-                        return isRead;
+myForm.addEventListener("submit", (e) => {
+        function addBookToLibrary(isRead) {
+                let author = document.getElementById('authorInput').value;
+                let title = document.getElementById('titleInput').value;
+                let pages = document.getElementById('pgNoInput').value;
+                function validate(){
+                        var remember = document.getElementById('completedInput');
+                        if (remember.checked){
+                                isRead = "on";
+                                return isRead;
+                        } else {
+                                isRead = "off";
+                                return isRead;
+                        }
                 }
-            }
-        validate();
-        let bookTest = new book(title,author, pages, isRead);
-        myLibrary.push(bookTest);
+                validate();
+                let bookTest = new book(title,author, pages, isRead);
+                myLibrary.push(bookTest);
+                document.querySelector('#authorInput').value = '';
+                document.querySelector('#titleInput').value = '';
+                document.querySelector('#titleInput').value = '';
+        }
+        addBookToLibrary(isRead);
+});
+
+function deleteBook0() {
+        document.getElementById("card0").style.display = "none";
 }
-
-function deleteBook() {
-
+function deleteBook1() {
+        document.getElementById("card1").style.display = "none";
 }
-
+function deleteBook2() {
+        document.getElementById("card2").style.display = "none";
+}
+function deleteBook3() {
+        document.getElementById("card3").style.display = "none";
+}
+function deleteBook4() {
+        document.getElementById("card4").style.display = "none";
+}
