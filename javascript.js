ae830451ee8da1beb06fa88c420d38b37fc94c3e
JFerrettUK <jferrettuk@gmail.com>
2022-09-17 14:22:53 +0100

    Library delete functionality added

--- javascript.js
+++ javascript.js
@@ -72,14 +72,8 @@ function loopMyLibrary(myLibrary, i) {
                         }
                 }
                 validate();
-                console.log(`deletePause${i}`);
                 function checkDeletion(i) {
-                        if (`deletePause${i} === "true"`) {
-                                document.getElementById(`card${i}`).style.display = "grid";  
-                        } else if (`deletePause${i} === "false"`) {
-                                document.getElementById(`card${i}`).style.display = "none";  
-
-                        }
+                        document.getElementById(`card${i}`).style.display = "grid";  
                 }
                 checkDeletion(i);
                 i++;
@@ -92,175 +86,102 @@ myForm.addEventListener("submit", (e) => {
         loopMyLibrary(myLibrary, i)
 });
 
-
-// function cardContent0(myLibrary, i) {
-//         let bookTitle0 = document.getElementById("bookTitle0");
-//         let titleText0 = document.createTextNode("This just got added");
-//         bookTitle0.appendChild(titleText0);
-
-//         let bookAuthor0 = document.getElementById("bookAuthor0");
-//         let authorText0 = document.createTextNode("This just got added");
-//         bookAuthor0.appendChild(authorText0);
-
-//         let pageNumber0 = document.getElementById("pageNumber0");
-//         let pageNoText0 = document.createTextNode("344");
-//         pageNumber0.appendChild(pageNoText0);
-
-//         function validate(){
-//                 let remember = document.getElementById('completedInput');
-//                 if (remember.checked){
-//                         document.getElementById("completedBox0").style.backgroundColor = "#143c79";
-//                         return isRead;
-//                 } else {
-//                         isRead = "off";
-//                         document.getElementById("completedBox0").style.backgroundColor = "lightgrey";
-//                 }
-//         }
-//         validate();
-// }
-
-// if (i === 0) {
-
-// }
-
-let deletePause0 = "false";
-let deletePause1 = "false";
-let deletePause2 = "false";
-let deletePause3 = "false";
-let deletePause4 = "false";
-let deletePause5 = "false";
-let deletePause6 = "false";
-let deletePause7 = "false";
-let deletePause8 = "false";
-let deletePause9 = "false";
-let deletePause10 = "false";
-let deletePause11 = "false";
-let deletePause12 = "false";
-let deletePause13 = "false";
-let deletePause14 = "false";
-let deletePause15 = "false";
-let deletePause16 = "false";
-let deletePause17 = "false";
-let deletePause18 = "false";
-let deletePause19 = "false";
-
-
 function deleteBook0() {
         document.getElementById("card0").style.display = "none";
-        deletePause0 = "true";
-        return deletePause0;
+        myLibrary.splice(0, 1);
 }
 
 function deleteBook1() {
         document.getElementById("card1").style.display = "none";
-        deletePause1 = "true";
-        return deletePause1;
+        myLibrary.splice(1, 1);
 }
 
 function deleteBook2() {
         document.getElementById("card2").style.display = "none";
-        deletePause2 = "true";
-        return deletePause2;
+        myLibrary.splice(2, 1);
 }
 
 function deleteBook3() {
-        document.getElementById("card3").style.display = "none";
-        deletePause3 = "true";
-        return deletePause3;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(3, 1);
 }
 
 function deleteBook4() {
-        document.getElementById("card4").style.display = "none";
-        deletePause4 = "true";
-        return deletePause4;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(4, 1);
 }
 
 function deleteBook5() {
-        document.getElementById("card5").style.display = "none";
-        deletePause5 = "true";
-        return deletePause5;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(5, 1);
 }
 
 function deleteBook6() {
-        document.getElementById("card6").style.display = "none";
-        deletePause6 = "true";
-        return deletePause6;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(6, 1);
 }
 
 function deleteBook7() {
-        document.getElementById("card7").style.display = "none";
-        deletePause7 = "true";
-        return deletePause7;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(7, 1);
 }
 
 function deleteBook8() {
-        document.getElementById("card8").style.display = "none";
-        deletePause8 = "true";
-        return deletePause8;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(8, 1);
 }
 
 function deleteBook9() {
-        document.getElementById("card9").style.display = "none";
-        deletePause9 = "true";
-        return deletePause9;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(9, 1);
 }
 
 function deleteBook10() {
-        document.getElementById("card10").style.display = "none";
-        deletePause10 = "true";
-        return deletePause10;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(10, 1);
 }
 
 function deleteBook11() {
-        document.getElementById("card11").style.display = "none";
-        deletePause11 = "true";
-        return deletePause11;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(11, 1);
 }
 
 function deleteBook12() {
-        document.getElementById("card12").style.display = "none";
-        deletePause12 = "true";
-        return deletePause12;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(12, 1);
 }
 
 function deleteBook13() {
-        document.getElementById("card13").style.display = "none";
-        deletePause13 = "true";
-        return deletePause13;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(13, 1);
 }
 
 function deleteBook14() {
-        document.getElementById("card14").style.display = "none";
-        deletePause14 = "true";
-        return deletePause14;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(14, 1);
 }
 
 function deleteBook15() {
-        document.getElementById("card15").style.display = "none";
-        deletePause15 = "true";
-        return deletePause15;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(15, 1);
 }
 
 function deleteBook16() {
-        document.getElementById("card16").style.display = "none";
-        deletePause16 = "true";
-        return deletePause16;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(16, 1);
 }
 
 function deleteBook17() {
-        document.getElementById("card17").style.display = "none";
-        deletePause17 = "true";
-        return deletePause17;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(17, 1);
 }
 
 function deleteBook18() {
-        document.getElementById("card18").style.display = "none";
-        deletePause18 = "true";
-        return deletePause18;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(18, 1);
 }
 
 function deleteBook19() {
-        document.getElementById("card19").style.display = "none";
-        deletePause19 = "true";
-        return deletePause19;
+        document.getElementById("card2").style.display = "none";
+        myLibrary.splice(19, 1);
 }
